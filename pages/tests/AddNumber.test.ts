1eb421e076ed9ff6817e8720a80863dc59b451f5
Lukas Zimmermann <lukas@MacBook-Air-von-Lukas.local>
2023-03-11 11:32:18 +0100

    fix action again

--- pages/tests/AddNumber.test.ts
+++ pages/tests/AddNumber.test.ts
@@ -3,6 +3,6 @@ import { describe, expect, it } from "vitest"
 
 describe('#Check AddNumber Function', () =>{
     it("Check if 2 plus 3 is equal 5", () =>{
-        expect(AddNumber.AddNumber(2,8)).toBe(5);
+        expect(AddNumber.AddNumber(2,3)).toBe(5);
     })
 })
